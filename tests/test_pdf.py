22011be4ffb59379642340b906b0b2cd82477c8d
SandonasFeliz <janelesandonas@outlook.com>
2026-03-10 13:05:52 +0100

    Rename PDF file path in test case

--- tests/test_pdf.py
+++ tests/test_pdf.py
@@ -4,7 +4,7 @@ import os
 class TestPDF(unittest.TestCase):
 
     def test_file_is_pdf(self):
-        file_path = "tests/data/sample_paper.pdf"
+        file_path = "tests/data/paper_sample.pdf"
 
         self.assertTrue(os.path.exists(file_path))
         self.assertTrue(file_path.lower().endswith(".pdf"))
